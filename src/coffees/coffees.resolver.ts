a6ffeac6b5c506cfdc48ecd928e5cfe76fd84294
Brian Lai <a1740942002@me.com>
2025-05-30 18:36:10 +0800

    Add createCoffee mutation and CreateCoffeeInput type
    - Implemented createCoffee mutation in CoffeesResolver to handle coffee creation.
    - Introduced CreateCoffeeInput class to define the input structure for creating a coffee.
    - Removed the outdated coffees.graphql schema file as it is no longer needed.

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,6 +1,6 @@
 import { ParseIntPipe } from '@nestjs/common';
-import { Resolver, Query, Args, ID } from '@nestjs/graphql';
-import { Coffee } from 'src/graphql/types';
+import { Resolver, Query, Args, ID, Mutation } from '@nestjs/graphql';
+import { Coffee, CreateCoffeeInput } from 'src/graphql/types';
 
 @Resolver()
 export class CoffeesResolver {
@@ -12,5 +12,15 @@ export class CoffeesResolver {
   @Query(() => Coffee, { name: 'coffee' })
   async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
     return null;
+  }
+
+  @Mutation('createCoffee') // notice we're decoupled the name from the actual GQL mutation name 'createCoffee'
+  async create(
+    @Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput,
+  ): Promise<Coffee> {
+    return {
+      ...createCoffeeInput,
+      id: 1,
+    };
   }
 }
