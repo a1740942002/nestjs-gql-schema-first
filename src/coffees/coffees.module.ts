2a2c2d5013701f9bb5e73a6ad22fd3489b20544b
Brian Lai <a1740942002@me.com>
2025-05-30 21:21:14 +0800

    Add CoffeeFlavorsResolver to CoffeesModule for enhanced flavor management

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,9 +4,10 @@ import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeEntity } from './entities/coffee.entity';
 import { FlavorEntity } from './entities/flavor.entity';
+import { CoffeeFlavorsResolver } from './coffee-flavors.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CoffeeEntity, FlavorEntity])],
-  providers: [CoffeesResolver, CoffeesService],
+  providers: [CoffeesResolver, CoffeesService, CoffeeFlavorsResolver],
 })
 export class CoffeesModule {}
